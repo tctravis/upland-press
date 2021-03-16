a537577654cdf94ac1e7c8cf6db66496d16d150c
Toby Travis <tobytravis@tobys-mbp.home>
2021-03-16 20:21:58 +0000

    Adding images through forestry

--- pages/prints/_slug.vue
+++ pages/prints/_slug.vue
@@ -2,6 +2,7 @@
   <article class="gap-top-500">
     <h1>{{ print.title }}</h1>
     <p>Price: {{ print.price }}</p>
+    <img :src="require(`~/assets/images${print.print_image}`)" />
     <nuxt-content :document="print" />
   </article>
 </template>
