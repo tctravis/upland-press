9b0bf523ff5bc19c0e6c4125b1eba6cc27ae87be
Toby Travis <tobytravis@tobys-mbp.home>
2021-04-30 16:00:40 +0100

    First go at laying out home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
   <MainContent>
-    <div class="[ Home__image ][ gap-bottom-500 ]">
-      <CloudinaryImage
-        v-if="page.main_image"
-        :src="page.main_image.image"
-        :alt="page.main_image.alt_text"
-        :src-set-widths="imageSrcSets"
-        :src-set-sizes="imageSizes"
-      />
-    </div>
-    <div class="[ Home__title ]">
-      <PageTitle>Linocut prints of the northern uplands</PageTitle>
-    </div>
-    <div class="[ Home__text ]">
-      <nuxt-content :document="page" />
+    <div class="[ Home ]">
+      <div class="[ Home__image ][ gap-bottom-500 ]">
+        <CloudinaryImage
+          v-if="page.main_image"
+          :src="page.main_image.image"
+          :alt="page.main_image.alt_text"
+          :src-set-widths="imageSrcSets"
+          :src-set-sizes="imageSizes"
+        />
+      </div>
+      <div class="[ Home__title ][ flow ]">
+        <PageTitle>Linocut prints of the northern uplands</PageTitle>
+        <p>by Toby Travis</p>
+      </div>
+      <div class="[ Home__text ]">
+        <nuxt-content :document="page" />
+      </div>
     </div>
   </MainContent>
 </template>
@@ -46,7 +49,7 @@ export default {
   },
   computed: {
     imageSrcSets() {
-      return [300, 500, 1000]
+      return [400, 500, 1000]
     },
     imageSizes() {
       return '(min-width:62em) 50vw, 70vw'
@@ -55,4 +58,52 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.Home {
+  --space: 1.2rem;
+  display: grid;
+  gap: var(--space);
+
+  grid-template-columns: 100%;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'image'
+    'title'
+    'text';
+  > * {
+    grid-column: 1 / -1;
+    grid-row: auto;
+    text-align: right;
+  }
+  // @include media-query('sm') {
+  // grid-template-columns: minmax(10px, 4fr) minmax(10px, 6fr);
+  // }
+  @include media-query('md') {
+    grid-template-columns: minmax(10px, 5fr) minmax(10px, 6fr);
+    grid-template-areas:
+      'title image'
+      '. text';
+  }
+  @include media-query('widescreen') {
+    grid-template-columns: minmax(10px, 5fr) minmax(10px, 6fr);
+    grid-template-areas:
+      'title image'
+      'text image';
+    .Home__image {
+      margin-bottom: 0;
+    }
+    .Home__text {
+      align-self: end;
+    }
+  }
+}
+.Home__image {
+  grid-area: image;
+}
+.Home__title {
+  grid-area: title;
+}
+.Home__text {
+  grid-area: text;
+}
+</style>
